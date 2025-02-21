a774a4093a98be812a5b9de7001cb267d6abc4e0
NabiaHaider2 <nabiahaider11@gmail.com>
2025-02-21 22:17:15 +0500

    Update page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import Footer from '../components/Footer';
+import Footer from '@/app/components/Footer';
 
 const AccountPage = () => {
   const [loginData, setLoginData] = useState({

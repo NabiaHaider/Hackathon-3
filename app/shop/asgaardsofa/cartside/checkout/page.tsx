74297cf227edc0c6b8d84be2c0df5d646049b457
NabiaHaider2 <nabiahaider11@gmail.com>
2025-02-21 19:31:49 +0500

    Update page.tsx

--- app/shop/asgaardsofa/cartside/checkout/page.tsx
+++ app/shop/asgaardsofa/cartside/checkout/page.tsx
@@ -4,6 +4,7 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import Footer from '@/app/components/Footer';
 
 const CheckOut = () => {
   const [formData, setFormData] = useState({
@@ -195,70 +196,7 @@ const CheckOut = () => {
         </div>
       </section>
 
-           {/* Footer */}
-           <footer className="bg-white py-8 border-t border-gray-200">
-        <div className="container mx-auto px-4 md:px-12">
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
-                {/* Address */}
-                <div className="text-gray-600 text-sm">
-                    <p>Jouhar</p>
-                    <p className='my-4'>Pakistan</p>
-                </div>
-
-                {/* Links */}
-                <div>
-                    <h3 className="text-gray-400 text-sm font-medium mb-3">Links</h3>
-                    <ul className="space-y-2">
-                        <li>
-                            <Link href="/" className="text-black hover:text-gray-700 transition">Home</Link>
-                        </li>
-                        <li>
-                            <Link href="/shop" className="text-black hover:text-gray-700 transition">Shop</Link>
-                        </li>
-                        <li>
-                            <Link href="#" className="text-black hover:text-gray-700 transition">About</Link>
-                        </li>
-                        <li>
-                            <Link href="/contact" className="text-black hover:text-gray-700 transition">Contact</Link>
-                        </li>
-                    </ul>
-                </div>
-
-                {/* Help */}
-                <div>
-                    <h3 className="text-gray-400 text-sm font-medium mb-3">Help</h3>
-                    <ul className='space-y-2'>
-                        <li>
-                            <Link href="#" className="text-black hover:text-gray-700 transition">Payment Options</Link>
-                        </li>
-                        <li>
-                            <Link href="#" className="text-black hover:text-gray-700 transition">Return</Link>
-                        </li>
-                        <li>
-                            <Link href="#" className="text-black hover:text-gray-700 transition">Privacy Policies</Link>
-                        </li>
-                    </ul>
-                </div>
-
-                {/* Newsletter */}
-                <div>
-                    <h3 className="text-gray-500 text-sm font-medium mb-3">Newsletter</h3>
-                    <div className="flex items-center">
-                        <input type="email" placeholder="Enter Your Email Address" className="flex-1 px-4 py-2 border-gray-300 rounded-l-md focus:outline-none focus:ring-1 focus:ring-gray-400" />
-                        <button className=" text-black underline  px-4 py-2 font-bold hover:bg-gray-300 transition">Subscribe</button>
-                    </div>
-                </div>
-            </div>
-
-            {/* Bottom Section */}
-            <div className="mt-8 text-center text-gray-600 text-sm">
-                <hr />
-            </div>
-            <div className="text-center mt-4 text-gray-500 text-sm">
-                <p>Designed by <span className="text-black">Nabia</span> | All Rights Reserved</p>
-            </div>
-        </div>
-    </footer>
+          <Footer />
     </div>
   );
 };

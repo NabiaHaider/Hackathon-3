6b0c2b6b4eb4d3fd7cfe83aa91cc166385bccf3f
NabiaHaider2 <nabiahaider11@gmail.com>
2025-02-21 22:19:30 +0500

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -4,7 +4,7 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Footer from "../components/Footer";
+import Footer from '@/app/components/Footer';
 
 interface Product {
   id: number;
